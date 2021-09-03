e0412629cc7c9661381d53409419fedaafe224bf
Zurdoo3 <axel@agencycoda.com>
2021-09-03 16:14:07 -0300

    Styles

--- projects/agencycoda/mia-billing/src/lib/entities/mia_method.ts
+++ projects/agencycoda/mia-billing/src/lib/entities/mia_method.ts
@@ -1,6 +1,6 @@
 export class MiaMethod {
 
-    static PAYPAL_METHOD: MiaMethod = { id: 'paypal', title: 'Pay with PayPal', image: 'paypal.png' };
+    static PAYPAL_METHOD: MiaMethod = { id: 'paypal', title: 'Pay with PayPal', image: '/assets/paypal.png' };
 
     id = '';
     title = '';
